f4535f7db643ad1706c07b219195dccb9b249742
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-05-27 01:26:59 +0000

    Add Db Config Files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_SERVER', '4audz.h.filess.io');
+define('DB_USER', 'cosc4806_fooddinner');
+define('DB_PASS', $ENV['DB_PASS']'');
+define('DB_DATABASE', 'cosc4806_fooddinner');
+define('DB_PORT', '61000')
+
+?>
