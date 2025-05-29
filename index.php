08d33375a02f6438ade9518d87dbacef56b6d51f
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-05-29 01:34:56 +0000

    Add User function

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+    require_once('user.php');
+    $user = new User();
+    $users_list = user->get_all_users();
+    print_r($users_list) 
+
+?>
 <html>
   <head>
     <title>PHP Test</title>
